6466e64df679e5af0e073f739aac3f4b5c52c472
0ldude <geniodelletartarughe@protonmail.com>
2022-04-15 16:19:48 +0200

    added emoji-highlight patch

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -6,18 +6,31 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+        "FiraCode Nerd Font:size=12",
 };
 static const char *prompt      = "Search:";      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+static const char *colors[SchemeLast][10] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeHp]  = { "#bbbbbb", "#333333" }
+	[SchemeHp]  = { "#bbbbbb", "#333333" },
+        [SchemeHighlight] = {"#f1fa8c", "#596377", "#3E485B"},
+        [SchemeHover] = {"#ffffff", "#353D4B", "#3E485B"},
+        [SchemeGreen] = {"#ffffff", "#52E067", "#41b252"},
+        [SchemeRed] = {"#ffffff", "#e05252", "#c24343"},
+        [SchemeYellow] = {"#ffffff", "#e0c452", "#bca33f"},
+        [SchemeBlue] = {"#ffffff", "#5280e0", "#3a62b3"},
+        [SchemePurple] = {"#ffffff", "#9952e0", "#7439b0"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 25;
+static unsigned int lineheight = 0; /* -h option; minimum height of a menu line     */
+static unsigned int min_lineheight = 8;
+static int sely = 0;
+static int commented = 0;
+static int animated = 0;
 
 /*
  * Characters not considered part of a word while deleting words
