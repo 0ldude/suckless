1a786ef9a089d492312f3ab397e5ee90cf4ac40e
0ldude <geniodelletartarughe@protonmail.com>
2022-05-17 14:53:57 +0200

    applied slock-background-image patch

--- slock/config.def.h
+++ slock/config.def.h
@@ -3,10 +3,13 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#282828",     /* after initialization */
+	[INPUT] =  "#458588",   /* during input */
+	[FAILED] = "#CC241D",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/home/yalo/Pictures/1920x1080-dark-archlinux2.png";
